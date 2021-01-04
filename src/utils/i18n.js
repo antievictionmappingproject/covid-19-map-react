5382a319c9b67454465baba79ca1cbc4236137aa
river <river@sk-river.sk.lan>
2021-01-04 19:22:39 +0000

    remove i18n function

--- src/utils/i18n.js
+++ src/utils/i18n.js
@@ -6,21 +6,6 @@ import store from '../reducers';
 
 import { languages } from './constants';
 
-export const I18N_ATTRIBUTE = 'data-i18n';
-
-export const translateContent = element => {
-  const selectedDomTree = element || document;
-  const elements = selectedDomTree.querySelectorAll(`[${I18N_ATTRIBUTE}]`);
-  elements.forEach(el => {
-    const key = el.dataset.i18n;
-    const t = i18next.t(key);
-    // If translation found set element content as translation.
-    if (t !== key) el.innerHTML = t;
-    // Otherwise use english as fallback
-    else el.innerHTML = i18next.t(key, { lng: 'en' });
-  });
-};
-
 // Default values for i18next config
 const i18nOptions = {
   whitelist: languages,
