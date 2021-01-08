2ebddc2c75df6358137c11023949feeedbe97732
Tim Hitchins <tim.x.hitchins@gmail.com>
2021-01-08 15:49:40 -0800

    WIP searchbar - ad debounce

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,49 +1,72 @@
-import React, { useState, useEffect } from 'react';
-import { getSearchData } from '../utils/data';
+import React, { useEffect, useCallback, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import debounce from 'lodash.debounce';
 import i18next from 'i18next';
 import { fetch } from 'whatwg-fetch';
 
-function debounce(func, wait, immediate) {
-  let timeout;
-  return function () {
-    const context = this,
-      args = arguments;
-    const later = function () {
-      timeout = null;
-      if (!immediate) func.apply(context, args);
-    };
-    const callNow = immediate && !timeout;
-    clearTimeout(timeout);
-    timeout = setTimeout(later, wait);
-    if (callNow) func.apply(context, args);
-  };
+const BING_API_KEY =
+  'AiCodebvKHCT2XAWYPvfOIkR9f8EA0AfLBnCmL2TchluJ3kn36befi0DWGzm9fuz';
+
+// async function fetchBingData(searchTerm) {
+//   const lang = navigator.language ? `&culture = ${navigator.language}` : '';
+//   const url = `https://dev.virtualearth.net/REST/v1/Autosuggest?query=${searchTerm.trim()}${lang}&includeEntityTypes=place&userMapView=-90,-180,90,180&key=${BING_API_KEY}`;
+//   try {
+//     const res = await fetch(url);
+//     const json = await res.json();
+//     console.log(json);
+//     return json;
+//   } catch (e) {
+//     throw new Error(`An error occurred fetching Bing data: ${e.message}.`);
+//   }
+// }
+
+function useDebounce(callback, delay) {
+  // Memoizing the callback because if it's an arrow function
+  // it would be different on each render
+  const memoizedCallback = useCallback(callback, []);
+  const debouncedFn = useRef(debounce(memoizedCallback, delay));
+
+  useEffect(() => {
+    debouncedFn.current = debounce(memoizedCallback, delay);
+  }, [memoizedCallback, debouncedFn, delay]);
+
+  return debouncedFn.current;
 }
 
-async function fetchBingData(searchTerm) {
-  const BING_API_KEY =
-    'Al2-1GXrd8GzwtSAID3J3LJn-flLLgNWzNtsT5nnSKW8dA2ClgaXXXMQR6WfE6wE';
-  const lang = navigator.language ? `&culture = ${navigator.language}` : '';
-  const url = `https://dev.virtualearth.net/REST/v1/Autosuggest?query=${"portland"}${lang}&includeEntityTypes=place&userMapView=-90,-180,90,180&key=${BING_API_KEY}`;
-  try {
-    const res = await fetch(url);
-    const json = await res.json();
-    return json;
-  } catch (e) {
-    throw new Error(`An error occurred fetching Bing data: ${e.message}.`);
-  }
+function useDebouncedFetch(callback, delay, term) {
+  const dispatch = useDispatch();
+  // Memoizing the callback because if it's an arrow function
+  // it would be different on each render
+  const memoizedCallback = useCallback(callback, []);
+  const debouncedFn = useRef(debounce(memoizedCallback, delay));
+
+  useEffect(() => {
+    debouncedFn.current = debounce(memoizedCallback, delay);
+  }, [memoizedCallback, debouncedFn, delay, term]);
 }
 
 function SearchBar(props) {
-  const { searchTerm } = useSelector(state => state.ui);
+  const { searchTerm, searchResults } = useSelector(state => state.ui);
   const dispatch = useDispatch();
+  // const debouncedFetch = useDebounce(val => fetchBingData(val), 1000);
 
-  const onChange = async e => {
-    dispatch({ type: 'ui:search:term:set', payload: e.target.value });
-    const data = debounce(fetchBingData(searchTerm), 300);
-    console.log(data);
+  const onChange = val => {
+    dispatch({ type: 'ui:search:term:set', payload: val });
   };
 
+  async function fetchBingData(term) {
+    const lang = navigator.language ? `&culture = ${navigator.language}` : '';
+    const url = `https://dev.virtualearth.net/REST/v1/Autosuggest?query=${term.trim()}${lang}&includeEntityTypes=place&userMapView=-90,-180,90,180&key=${BING_API_KEY}`;
+    try {
+      const res = await fetch(url);
+      const json = await res.json();
+      dispatch({ type: 'ui:search:results:set', payload: json });
+    } catch (e) {
+      throw new Error(`An error occurred fetching Bing data: ${e.message}.`);
+    }
+  }
+
+  const debouncedOnChange = useDebounce(val => fetchBingData(val), 1000);
   const onSubmit = e => {
     e.preventDefault();
   };
@@ -52,7 +75,10 @@ function SearchBar(props) {
     <form id="search-bar-form" onSubmit={e => onSubmit(e)}>
       <div id="search-bar-div">
         <input
-          onChange={e => onChange(e)}
+          onChange={async e => {
+            onChange(e.target.value);
+            debouncedOnChange(e.target.value);
+          }}
           type="text"
           name="search"
           id="search-bar"
