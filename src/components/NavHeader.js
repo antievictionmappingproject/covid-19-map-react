1b66a20e7452561981952f8218f7eeba80ca39c0
Bora Erden <berden@situ.nyc>
2021-10-05 23:20:52 -0400

    css changes to make mobile responsive

--- src/components/NavHeader.js
+++ src/components/NavHeader.js
@@ -1,30 +1,44 @@
 import React, { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
-// import HamburgerMenu from './HamburgerMenu';
+import HamburgerMenu from './HamburgerMenu';
 
 const NavHeader = () => {
-  const path = useLocation().pathname
+  const path = useLocation().pathname;
 
   return (
     <div id="navHeader">
-      {/*<HamburgerMenu />*/}
+      {/* <HamburgerMenu /> */}
       <div id="mainHeader">
         <a href="https://covid19.antievictionmap.com/">
-        THE ANTI-EVICTION MAPPING PROJECT | COVID-19
+          THE ANTI-EVICTION MAPPING PROJECT | COVID-19
         </a>
       </div>
       <div id="mapLinks">
-        <div id="housingProtectionLink" className={`link-cnr ${path === '/tenant-protections' ? 'active' : ''}`}>
-          <Link to="/tenant-protections"> COVID-19    GLOBAL    HOUSING    PROTECTION<br />
-            LEGISLATION & HOUSING JUSTICE ACTION</Link>
+        <div
+          id="housingProtectionLink"
+          className={`link-cnr ${
+            path === '/tenant-protections' ? 'active' : ''
+          }`}
+        >
+          <Link to="/tenant-protections">
+            {' '}
+            COVID-19 GLOBAL HOUSING PROTECTION
+            <br />
+            LEGISLATION & HOUSING JUSTICE ACTION
+          </Link>
         </div>
-        <div id="oralHxtoriesLink" className={`link-cnr ${path === '/eviction-stories' ? 'active' : ''}`}>
-          <Link to="/eviction-stories">COVID-19   ORAL   HXSTORIES   OF   TENANT <br />
-            RESISTANCE & EVICTION INFORMATION</Link>
+        <div
+          id="oralHxtoriesLink"
+          className={`link-cnr ${path === '/eviction-stories' ? 'active' : ''}`}
+        >
+          <Link to="/eviction-stories">
+            COVID-19 ORAL HXSTORIES OF TENANT <br />
+            RESISTANCE & EVICTION INFORMATION
+          </Link>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default NavHeader
+export default NavHeader;
